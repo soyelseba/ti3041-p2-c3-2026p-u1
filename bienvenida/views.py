445ead6b61fec1eb4701a82ee6a8af80b8705ab8
Sebastian Morales <51178433+soyelseba@users.noreply.github.com>
2026-08-25 13:28:22 -0400

    Agregar vista y ruta de productos
    Se incorpora una vista que consulta todos los `Producto` y los renderiza en una nueva plantilla de listado. Tambien se registra la URL `lista_productos/` para exponer esta pagina desde el proyecto.

--- bienvenida/views.py
+++ bienvenida/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Producto
+
 # Create your views here.
 def inicio(request):
     return HttpResponse("Hola mundo desde Django")
+
+def lista_productos(request):
+    productos = Producto.objects.all()
+    return render(request, 'productos/lista.html', {'productos': productos} )
